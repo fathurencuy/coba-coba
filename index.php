57420ae80c8fb2172e84134d5ab8882070b6bc66
fathurencuy <rochmanfathur606@gmail.com>
2025-03-23 16:14:51 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, id nihil, a distinctio quia veritatis dolorum, ipsum hic voluptas ad facere saepe iste sit dicta! Vel ducimus accusantium corrupti natus.</p>
 </body>
 </html>
