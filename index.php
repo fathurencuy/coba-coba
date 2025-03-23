fef51ea359083c8d0110dc75a3b4b3f53884ebe6
fathurencuy <rochmanfathur606@gmail.com>
2025-03-23 16:20:33 +0700

    menambah paragraf baru

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, id nihil, a distinctio quia veritatis dolorum, ipsum hic voluptas ad facere saepe iste sit dicta! Vel ducimus accusantium corrupti natus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis impedit qui eaque illo molestiae nam quis iusto quo! Nemo provident ipsum nostrum eum tempora autem ad, veritatis illo reiciendis cumque?</p>
 </body>
 </html>
